fb55a1a6c9d6d5ed1ad2a6cc699e8f4dd5d332ba
dararod <dalejorod@gmail.com>
2024-11-07 15:02:22 -0400

    feat: implement testing database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import dotEnv from 'dotenv'
 
 import config from './config';
 import { router } from './router';
 import cookieParser from 'cookie-parser';
-
 async function serve(): Promise<void> {
+  dotEnv.config();
+  console.log(process.env.DATABASE_URL);
   const app = express();
   const routes = router();
 
