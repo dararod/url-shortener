1085ce3c6f7968deefe4c7402f1f4edce301a2d6
dararod <dalejorod@gmail.com>
2024-11-07 16:29:55 -0400

    feat: capability to activate and deactivate links

--- server/src/router/api/v1/links.ts
+++ server/src/router/api/v1/links.ts
@@ -13,6 +13,7 @@ export function links(): Router {
   router.post("/", async (req, res) => {
     try {
       await openMongoDBConn();
+      verifyToken(req);
 
       const reqBody = req.body;
 
@@ -24,6 +25,8 @@ export function links(): Router {
       const entry = new UrlModel({
         fullUrl: reqBody.fullUrl,
         shortUrl: reqBody.shortUrl,
+        activatedAt: Date.now(),
+        deactivatedAt: null,
         createdAt: Date.now(),
         updatedAt: Date.now(),
       });
@@ -34,6 +37,8 @@ export function links(): Router {
         id: document._id,
         fullUrl: document.fullUrl,
         shortUrl: document.shortUrl,
+        activatedAt: document.activatedAt,
+        deactivatedAt: document.deactivatedAt,
         createdAt: document.createdAt,
         updatedAt: document.updatedAt,
       });
@@ -80,6 +85,52 @@ export function links(): Router {
           $set: {
             fullUrl: reqBody.fullUrl,
             shortUrl: reqBody.shortUrl,
+            updatedAt: new Date()
+          }
+        },
+        {new: true}
+      );
+
+      res.status(200).json(updatedLink)
+    } catch (err) {
+      return res.status(500).json({ message: (err as Error).message });
+    }
+  });
+
+  router.patch("/deactivate/:id", async (req, res) => {
+    try {
+      await openMongoDBConn();
+      verifyToken(req);
+      
+      const linkId = req.params.id
+      const updatedLink = await UrlModel.findByIdAndUpdate(
+        linkId,
+        {
+          $set: {
+            deactivatedAt: new Date(),
+            updatedAt: new Date()
+          }
+        },
+        {new: true}
+      );
+
+      res.status(200).json(updatedLink)
+    } catch (err) {
+      return res.status(500).json({ message: (err as Error).message });
+    }
+  });
+
+  router.patch("/activate/:id", async (req, res) => {
+    try {
+      await openMongoDBConn();
+      verifyToken(req);
+      
+      const linkId = req.params.id
+      const updatedLink = await UrlModel.findByIdAndUpdate(
+        linkId,
+        {
+          $set: {
+            activatedAt: new Date(),
             updatedAt: new Date()
           }
         },
