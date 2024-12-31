04e3671979798c147990c1d95d397c77f787b5dc
Esteban Borai <estebanborai@gmail.com>
2024-12-30 22:10:30 -0300

    feat(client): introduce client

--- packages/server/src/infra/repository/index.ts
+++ packages/server/src/infra/repository/index.ts
@@ -9,3 +9,11 @@ export type { Database, IRepository } from "./IRepository";
 export async function makeDatabaseConn(databaseURL: string): Promise<Database> {
   return connect(databaseURL);
 }
+
+export async function migrate(db: Database): Promise<void> {
+  const usersCollection = db.connection.collection('users');
+  await usersCollection.createIndex({ email: 1 }, { unique: true });
+
+  const links = db.connection.collection('links');
+  await links.createIndex({ slug: 1 }, { unique: true });
+}
